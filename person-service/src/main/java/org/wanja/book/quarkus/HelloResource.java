9593bffdc608020b8a1763d4649fbce8c256b3c6
wpernath <w.pernath@gmx.de>
2021-10-15 13:49:42 +0200

    added config property support for hello service

--- person-service/src/main/java/org/wanja/book/quarkus/HelloResource.java
+++ person-service/src/main/java/org/wanja/book/quarkus/HelloResource.java
@@ -5,12 +5,16 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 @Path("/hello")
 public class HelloResource {
+    @ConfigProperty(name = "app.greeting")
+    String greeting;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy";
+        return greeting;
     }
 }
