a7830968aa6e3da43414ed9edef481eb3d272885
wpernath <w.pernath@gmx.de>
2021-10-17 09:21:23 +0200

    Fixed test

--- person-service/src/test/java/org/wanja/book/quarkus/HelloResourceTest.java
+++ person-service/src/test/java/org/wanja/book/quarkus/HelloResourceTest.java
@@ -6,8 +6,13 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 @QuarkusTest
 public class HelloResourceTest {
+
+    @ConfigProperty(name = "app.greeting")
+    String greeting;
 
     @Test
     public void testHelloEndpoint() {
@@ -15,7 +20,7 @@ public class HelloResourceTest {
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("Hello RESTEasy"));
+             .body(is(greeting));
     }
 
 }
